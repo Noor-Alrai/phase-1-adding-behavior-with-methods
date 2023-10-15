9daab7086bcbf68ff811e98f978145e6cc6577b7
Noor Alrai <ameinnoor91@gmail.com>
2023-10-15 10:59:38 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+class Cat{
+    constructor(name,sex){
+        this.name=name;
+        this.sex=sex;
+    }
+    speak () {
+        return `${this.name} says meow!`
+     }
+}
+let myCat = new Cat("rose" , "female");
+console.log(myCat.speak())
+class Dog{
+    constructor(name,sex){
+        this.name=name;
+        this.sex=sex;
+    }
+       speak(){
+        return `${this.name} says woof!`
+ }
+}
+class Bird{
+    constructor(name,sex){
+        this.name=name;
+        this.sex=sex;
+    }
+    speak(sex){
+        if(this.sex === "male"){
+            return `It's me! ${this.name}, the parrot!`
+        }
+        else{ return `${this.name} says squawk!`}
+       
+     }
+
+
+}
+let bird2= new Bird ("Mable", "female");
+console.log(bird2.speak())
